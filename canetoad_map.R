224e5bb7eb249fd8d87fecea2fa080000581e986
Sho Rapley <rapleysho@gmail.com>
2024-02-13 07:38:40 +1100

    New repository
    Moved files from PhD git to new aus env git

---
+++ canetoad_map.R
@@ -0,0 +1,82 @@
+# cane toad spread map 2023 report
+setwd("C:/Users/shosh/OneDrive/1 Ecology work/AusEnv 2023")
+
+# library
+pacman::p_load(galah, janitor, ozmaps, sf, smoothr, terra, tidyterra, tidyverse)
+
+# background map
+oz <- ozmap("states") %>%
+  vect() %>%
+  project(crs("EPSG:4326"))
+
+# galah config
+galah_config(email = "shoshana.rapley@anu.edu.au")
+
+# call data
+toads_ala <- galah_call() %>%
+  galah_identify("Rhinella marina") %>%
+  galah_select(group = "basic",
+               basisOfRecord,
+               coordinateUncertaintyInMeters,
+               occurrenceStatus) %>%
+  atlas_occurrences() %>%
+  clean_names()
+
+# save ala data so I dont need to repeat the download call
+write.csv(toads_ala, "Rhinella_marina_ala.csv", row.names = FALSE)
+
+# apply additional filtering 
+toads <- read.csv("Rhinella_marina_ala.csv") %>%
+  filter(occurrence_status == "PRESENT", # remove absences
+         basis_of_record %in% c("OCCURRENCE", "OBSERVATION", #obs only
+                                "HUMAN_OBSERVATION","MACHINE_OBSERVATION"),
+         !is.na(event_date), #remove no date
+         !is.na(decimal_longitude),
+         !is.na(decimal_latitude),
+         !str_ends(1, pattern = "000"), # remove dubious coords
+         !str_ends(2, pattern = "000"),
+         !coordinate_uncertainty_in_meters>2000) %>% # remove >2km uncertainty
+  mutate(year = year(event_date))
+
+# rasterise by time bin, convert to concave hull and smooth range map
+base_raster <- rast(ncol=62.5, nrow=52.3, xmin=105.5, xmax=168, ymin=-43, ymax=-9.3)
+ch_ratio <- c(.25, .22, .15, .11)
+yearA <- c(1970, 2000, 2020, 2024)
+yearB <- c(1900, 1970, 2000, 2020)
+toad_maps <- data.frame()
+
+for(i in 1:4){
+  temp <- subset(toads, year<yearA[i] & year>=yearB[i]) %>%
+    select(c(5,4)) %>%
+    data.matrix() %>%
+    rasterize(base_raster, fun = "max") %>%
+    as.polygons() %>%
+    st_as_sf() %>%
+    drop_crumbs(threshold = units::set_units(10000, km^2)) %>% # drop single cell polygons
+    st_concave_hull(ratio = ch_ratio[i]) %>% # variable concave hull ratio by time period
+    smooth(method = "ksmooth") %>%
+    vect() %>%
+    crop(oz) %>%
+    st_as_sf()  %>%
+    mutate(bin = paste("pre", yearA[i], sep = "_"),
+           factor = i ) %>%
+    select(!max)
+  
+  toad_maps <- rbind(toad_maps, temp)
+}
+
+toad_maps <- arrange(toad_maps, -factor)
+
+# plot
+ggplot()+
+  geom_sf(data = toad_maps, aes(fill = bin), colour = NA)+
+  geom_sf(data = oz, fill = NA)+
+  theme_void()+
+  theme(legend.position = c(.85, .5))+
+  #geom_point(data = toads, aes(decimal_longitude, decimal_latitude))
+  scale_fill_manual(values = c("wheat", "tan2", "darkorange3","brown"),
+                     labels = c("Pre 1970", "1970-1999",
+                                "2000-2019", "2020-2023"))+
+  labs(fill = "Time Period")
+  
+
---
+++ epbc_lists.R
@@ -0,0 +1,101 @@
+# EPBC list wrangling for Aus Env Biodiversity report
+# last updated 30/01/2024 Shoshana Rapley
+
+# set up
+pacman::p_load(janitor, tidyverse, readxl)
+setwd("C:/Users/shosh/OneDrive/1 Ecology work/AusEnv 2023")
+
+# notes on data sources:
+#' Current list (29/1/24): manually formatted in long format; I used the currently accepted 
+#' sci name when two were listed and removed naming authority.
+#' flora: https://www.environment.gov.au/cgi-bin/sprat/public/publicthreatenedlist.pl?wanted=flora 
+#' fauna: https://www.environment.gov.au/cgi-bin/sprat/public/publicthreatenedlist.pl?wanted=fauna
+#' 
+#' Changes to the list (29/1/24): 'text to columns' function in excel used to split common and binomial names
+#' https://www.environment.gov.au/cgi-tmp/publiclistchanges.309265b1b3529d5bf13e.html 
+#' 
+#' Removals from the lis (1/2/22): "true" removals i.e. not taxonomic or info reasons were
+#' manually sorted by reading the listings
+#' https://www.environment.gov.au/cgi-tmp/publiclistchanges.407465b1b51cc727df3e.html
+
+# Make table for initial listings of later removed species
+initial <- read_excel("epbc_listings.xlsx", sheet = "Removals") %>%
+  clean_names() %>%
+  subset(grepl("Increase", reason) | grepl("Relisted", reason)) %>%
+  select(c("group","species","name","previous_status","year_first_listed")) %>%
+  rename(status = previous_status) %>%
+  rename(effective = year_first_listed) %>%
+  # add an initial listing date
+  mutate(effective = gsub("2000",as_date("16/07/2000", format = "%d/%m/%Y"), effective))
+
+# Make table for initial listings of later transferred species
+transfers <- read_excel("epbc_listings.xlsx", sheet = "Changes") %>%
+  clean_names() %>%
+  filter(str_detect(status, "Transfer")) %>%
+  mutate(status = word(status, start=3L)) %>%
+  mutate(status = ifelse(str_detect(status, "Critically"),
+                         "Critically Endangered", status)) %>%
+  mutate(status = ifelse(str_detect(status, "Conservation"),
+                         "Conservation Dependent", status)) %>%
+  mutate(effective = as_date("16/07/2000", format = "%d/%m/%Y"))
+
+# Join all data
+epbc <- read_excel("epbc_listings.xlsx", sheet = "Fauna") %>%
+  rbind(read_excel("epbc_listings.xlsx", sheet = "Flora")) %>%
+  rbind(read_excel("epbc_listings.xlsx", sheet = "Changes")) %>%
+  rbind(read_excel("epbc_listings.xlsx", sheet = "Delist")) %>%
+  clean_names() %>%
+  rbind(transfers) %>%
+  rbind(initial) %>%
+  arrange(species, status) %>%
+  mutate(effective = as_date(effective)) %>%
+  # standardise nomenclature and syntax, remove whitespace
+  mutate(species = str_trim(species,side = c("both")))
+
+# Lost some group data due to non standing naming so retrieving it here
+group <- select(epbc, c(group, species)) %>% arrange(group) %>%
+  filter(!duplicated(species)) %>% arrange(species)
+  
+epbc <- right_join(group, select(epbc, -(group)), by= "species") %>%
+# Remove duplicates where transferred
+  mutate(order = ifelse(str_detect(status, "Transfer"),1,2)) %>%
+  arrange(order) %>%
+  filter(!duplicated(cbind(species, effective))) %>%
+  arrange(species) %>%
+# Remove false initial listings
+  arrange(desc(effective)) %>%
+  filter(!duplicated(cbind(species, status))) %>%
+  arrange(species) %>%
+# Create column for year listed
+  mutate(year = year(effective)) %>%
+# Create column for listing type 
+  mutate(listing = ifelse(grepl("to Extinct",status),"Transfer extinct",
+                   ifelse(grepl("Transfer from Extinct",status),"Rediscovered",
+                   ifelse(grepl("Extinct",status),"Extinct",
+                   ifelse(grepl("Transfer",status),"Transfer",
+                   ifelse(grepl("Delisted",status),"Delisted", "Listed")))))) %>%
+  arrange(status) %>%
+  subset(select = -c(order))
+
+# Export 
+write.csv(epbc, "epbc_processed_2023.csv", row.names = FALSE)
+
+# Manual tasks in exported file:
+#' 1) Check correct listings of the 2 species that were delisted and relisted
+#' which seems to mess with the system:
+#' a) Macroderma gigas	Ghost Bat (true de and re listing)
+#' Mammal	Macroderma gigas	Vulnerable	Ghost Bat	16/07/2000	2000	Listed
+#' b) Amytornis textilis myall	Western Grasswren (true de and re listing)
+#' Bird	Amytornis textilis myall	Vulnerable	Western Grasswren 	16/07/2000	2000	Listed
+#' 
+#' 2) Check listing of CI Pipistrelle as there seems to be a timeline issue in the official data;
+#' should go endangered 2001 -> uplist CI 2006 -> uplist extinct 2021
+#' 
+#' 3) Manually check for duplicated common names (using conditional formatting) to find taxonomic synonyms to combine;
+#' usually occurs for species that were transferred but originally listed after 2000. Usually the initial listing
+#' has the outdated name (and therefore the group hasn't been brought across for the new name either).
+#' 
+#' 4) Check duplicated listings in species where naming isn't consistent on the listings:
+#' a) Genoplesium vernale AKA Corunastylis vernalis
+#' b) Galaxias truttaceus (Western Australian population)
+#' c) Phascolarctos cinereus (combined populations of Qld, NSW and the ACT)
